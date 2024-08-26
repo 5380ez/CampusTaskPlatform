b97a1b00698270c20a1eca7beffd7a0c22616442
maken-jhd <2065343055@qq.com>
2024-08-26 12:43:49 +0800

    小修一下

--- src/main/java/com/wuyanteam/campustaskplatform/entity/UserDTO.java
+++ src/main/java/com/wuyanteam/campustaskplatform/entity/UserDTO.java
@@ -8,17 +8,25 @@ import java.sql.Timestamp;
 public class UserDTO {
     private int id;
     private String username;
+    private String password;
     private String sex;
     private Integer age;
+    private Timestamp accCrtTime;
     private int stuId;
     private int exp;
     private int level;
     private int likeCount;
+    private String realName;
+    private String address;
+    private float balance;
+    private Timestamp lastLoginTime;
+    private int takeNum;
     private int publishNum;
     private String qq;
     private String email;
     private String phone;
-    private byte[] avatar;//头像
+    private int finishNum;
+    private byte[] avatar;
     private String signature;
     private byte[] photo;//照片墙
 }
