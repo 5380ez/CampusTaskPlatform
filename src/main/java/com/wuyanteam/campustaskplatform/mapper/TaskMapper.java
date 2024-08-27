30095af21b897d962f6d943f29cf75e1102dcd34
maken-jhd <2065343055@qq.com>
2024-08-27 21:53:05 +0800

    小小改

--- src/main/java/com/wuyanteam/campustaskplatform/mapper/TaskMapper.java
+++ src/main/java/com/wuyanteam/campustaskplatform/mapper/TaskMapper.java
@@ -2,7 +2,6 @@ package com.wuyanteam.campustaskplatform.mapper;
 
 import com.github.yulichang.base.MPJBaseMapper;
 import com.wuyanteam.campustaskplatform.entity.Task;
-import com.wuyanteam.campustaskplatform.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
