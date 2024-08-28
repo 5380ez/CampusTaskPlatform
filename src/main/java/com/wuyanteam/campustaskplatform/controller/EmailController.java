2cc0539dc1d2faef4f613da16bec6111b215afc1
jjjjmmmmeeee <2533523804@qq.com>
2024-08-28 21:46:39 +0800

    2

--- src/main/java/com/wuyanteam/campustaskplatform/controller/EmailController.java
+++ src/main/java/com/wuyanteam/campustaskplatform/controller/EmailController.java
@@ -1,5 +1,6 @@
 package com.wuyanteam.campustaskplatform.controller;
 
+import com.wuyanteam.campustaskplatform.Reposity.UserDao;
 import com.wuyanteam.campustaskplatform.service.ServiceImpl.MailServiceImpl;
 import com.wuyanteam.campustaskplatform.utils.Result;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,9 +16,24 @@ import javax.mail.MessagingException;
 public class EmailController {
     @Resource
     private MailServiceImpl mailService;
+    @Resource
+    private UserDao userDao;
 
     @GetMapping(value = "/sendEmail/{email}")
     public Result<Object> sendCode(@PathVariable String email) {
+        try {
+            mailService.mail(email);
+            return Result.success("验证码发送成功！");
+        } catch (MessagingException e) {
+            return Result.error("发送邮件失败，请稍后再试");
+        }
+    }
+
+    @GetMapping(value = "/sendEmailRegister/{email}")
+    public Result<Object> sendCode2(@PathVariable String email) {
+        if (userDao.findByEmail(email) != null) {
+            return Result.error("该邮件已被注册");
+        }
         try {
             mailService.mail(email);
             return Result.success("验证码发送成功！");
